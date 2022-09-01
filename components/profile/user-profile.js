1522b0aeea8f2988b93dc73f44fcb2835222ec7c
Vivek <vivek9142@gmail.com>
2022-09-02 03:53:33 +0530

    added ssr in profile page to check session and render profile or redirect to auth accordingly

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,5 +1,5 @@
-import { useSession, getSession } from "next-auth/client";
-import { useState, useEffect } from "react";
+// import { useSession, getSession } from "next-auth/client";
+// import { useState, useEffect } from "react";
 
 import ProfileForm from "./profile-form";
 import classes from "./user-profile.module.css";
@@ -8,24 +8,24 @@ function UserProfile() {
   // Redirect away if NOT auth
   // const [session, loading] = useSession();
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [loadedSession, setLoadingSession] = useState();
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [loadedSession, setLoadingSession] = useState();
 
-  useEffect(() => {
-    getSession().then((session) => {
-      if (!session) {
-        window.location.href = "/auth";
-      } else {
-        setIsLoading(false);
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     if (!session) {
+  //       window.location.href = "/auth";
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   });
+  // }, []);
 
   //using useSession will load the component endlessely
   //so we're doing workaround with getSession
-  if (isLoading) {
-    return <p className={classes.profile}>Loading...</p>;
-  }
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>;
+  // }
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,45 @@
-import UserProfile from '../components/profile/user-profile';
+import { getSession, unstable_getServerSession } from "next-auth/client";
+import UserProfile from "../components/profile/user-profile";
 
-function ProfilePage() {
+function ProfilePage(props) {
+  console.log(props);
   return <UserProfile />;
 }
 
 export default ProfilePage;
+
+/*
+brief moment of loading, which flashes on the screen when we enter profile when not 
+authenticated that is maybe something we want to get rid of.
+
+But we must not forget that Next.js blends server side and client side code, so we can 
+use server side code to determine whether the user, who sent the request, is authenticated 
+or not and return different page content and possibly a redirect if the user is not 
+authenticated.
+For this, we need to go to the profile page and then add an extra function.
+*/
+
+export async function getServerSideProps(context) {
+  /*
+  getSession, the good thing about this function is that you're not limited to using it on 
+  client side code only.You can, as you see, it works in a component.
+  */
+  const session = await getSession({ req: context.req });
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+
+  /*
+  So, now we use getServerSideProps for session tracking. And that means that in the user 
+  profile component we can now get rid of our client side code,
+  */
+}
